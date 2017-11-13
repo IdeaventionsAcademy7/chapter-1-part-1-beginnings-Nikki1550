c5038ef207d40339549141867e05fd25bea92ce2
Nikki1550 <32400853+Nikki1550@users.noreply.github.com>
2017-11-13 16:36:01 -0500

    number

--- ch1_09.py
+++ ch1_09.py
@@ -1,3 +1,12 @@
-# Name
-# Problem #
+# Nikki
+# Problem #9
 # Chapter 1 Problem Set Part 1
+
+number_str = input ("Choose a number:")
+number_int = int (number_str)
+
+number_int = (number_int + 2)
+number_int = (number_int * 3)
+number_int = (number_int - 6)
+number_int = (number_int / 3)
+print (number_int)
--- ch1_10.py
+++ ch1_10.py
@@ -1,3 +1,12 @@
-# Name
-# Problem #
+# Nikki
+# Problem #10
 # Chapter 1 Problem Set Part 1
+
+wives = 7
+sacks = wives*7
+cats = sacks*7
+kittens = cats*7
+
+print (wives + sacks + cats + kittens + 1)
+
+
