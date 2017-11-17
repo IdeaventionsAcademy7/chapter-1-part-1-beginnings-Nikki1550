12ef17a2f8ae22198b043389326d49c1105258e5
Nikki1550 <32400853+Nikki1550@users.noreply.github.com>
2017-11-17 15:47:59 -0500

    great lakes

--- ch1_Project_GreatLakes.py
+++ ch1_Project_GreatLakes.py
@@ -1,3 +1,14 @@
-# Name
+# Nikki
 # Project: Great Lakes are how big?
 # Chapter 1 Problem Set Part 1
+
+
+volume_gl = 22810
+area_usa = 8080464.3
+
+answer = volume_gl/area_usa
+answer = answer*1000
+
+print ("The area of the Great Lakes spread across the contiguous US states is:" )
+print (answer)
+print ("Meters")
