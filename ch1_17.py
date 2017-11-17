2b86fc3314fe361323a6552d7e8cbc486c42d764
Nikki1550 <32400853+Nikki1550@users.noreply.github.com>
2017-11-17 15:16:16 -0500

    input

--- ch1_17.py
+++ ch1_17.py
@@ -1,3 +1,11 @@
-# Name
-# Problem #
+# Nikki
+# Problem #17
 # Chapter 1 Problem Set Part 1
+
+
+something_str = input ("Enter something: ")
+print (something_str)
+something_int = int (something_str)
+print (something_int)
+something_float = float (something_str)
+print (something_float)
