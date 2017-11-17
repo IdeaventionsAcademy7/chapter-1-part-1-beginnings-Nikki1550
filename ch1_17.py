2a442464f11e3a22b4c8e2c80c17cabff0b5d8a1
Nikki1550 <32400853+Nikki1550@users.noreply.github.com>
2017-11-17 15:24:16 -0500

    input

--- ch1_17.py
+++ ch1_17.py
@@ -2,10 +2,16 @@
 # Problem #17
 # Chapter 1 Problem Set Part 1
 
-
 something_str = input ("Enter something: ")
 print (something_str)
 something_int = int (something_str)
 print (something_int)
 something_float = float (something_str)
 print (something_float)
+
+# Answers: 
+# You can input positive ingeters and print without an error because it can be
+# printed out as a string, integer, and a float.
+
+# You cannot print out any letters without getting an error of invalid literal
+# for int() or float() because they can't convert a letter into an integer. 
